9ea9a77dc83b94e4f558d6e944ed43a5e8f93ba1
vohaha <vohaha2011@gmail.com>
2015-04-23 11:25:39 +0300

    add Transaction page

--- assets/js/4com_transaction_list.js
+++ assets/js/4com_transaction_list.js
@@ -1,4 +1,4 @@
-var product_list = function () {
+var transaction_list = function () {
 	"use strict";
 	//function to initiate DataTable
 	//DataTable is a highly flexible tool, based upon the foundations of progressive enhancement,
@@ -6,7 +6,7 @@ var product_list = function () {
 	//For more information, please visit https://datatables.net/
 	var runDataTable_example1 = function () {
 
-		var oTable = $('#table-products').dataTable({
+		var oTable = $('#table-transaction').dataTable({
 			"bLengthChange": false,
 			"bPaginate": false,
 			"bProcessing": true,
@@ -19,13 +19,10 @@ var product_list = function () {
 					"sPrevious" : "",
 					"sNext" : ""
 				}
-			},
-			"aaSorting" : [[0, 'asc']],
-			// set the initial value
-			"iDisplayLength" : 10
+			}
 		});
 		// add placeholder to search field
-		$('#table-products_wrapper .dataTables_filter input').addClass("form-control input-sm").attr("placeholder", "Search");
+		$('#table-transaction_wrapper .dataTables_filter input').addClass("form-control input-sm").attr("placeholder", "Search");
 	};
 	return {
 		//main function to initiate template pages
